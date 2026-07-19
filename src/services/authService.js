4aa8932933a4c595761195be502059bbc4476f27
jallilavanya <152406275+jallilavanya@users.noreply.github.com>
2026-07-19 14:10:27 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,10 @@
 export async function signIn(email, password) {
- const response = await fetch('https://your-api-url/api/login', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ email, password }),
-});
+  const response = await fetch('http://localhost:5000/api/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password })
+  })
+
   const data = await response.json()
 
   if (!response.ok) {
